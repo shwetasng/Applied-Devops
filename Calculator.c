8161f80ba2f912eb4fb85a01a4a0fab6720b957e
shwetasng <shwetasng17@gmail.com>
2024-02-15 01:20:53 +0530

    line added in feature_branch-1

--- Calculator.c
+++ Calculator.c
@@ -5,6 +5,7 @@ int main() {
   char op;
   double first, second;
 printf("New line added in feature branch");
+printf("New line added in feature branch-1");
   printf("Enter an operator (+, -, *, /): ");
   scanf("%c", &op);
   printf("Enter two operands: ");
